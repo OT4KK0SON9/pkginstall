d653ab492b9744cd9e44e95c4ac4b4792a08ae3b
Kosong <160365589+OT4KK0SON9@users.noreply.github.com>
2024-03-15 17:06:00 +0700

    Add files via upload
    Kasih Bintang Banh Capek Tangan Gw

---
+++ src/full.sh
@@ -0,0 +1,322 @@
+echo "Pastikan Memakai Wifi ! Karena Ini Membutuhkan Banyak Kuota Dan Juga Ini Memakan Memory !"
+echo "."
+echo "."
+echo "tekan ctrl+c atau ctrl+z untuk membatalkan"
+echo "."
+echo "."
+echo "."
+echo "."
+echo "."
+echo "."
+echo "."
+echo "."
+echo "."
+echo "."
+echo "."
+echo "."
+echo "."
+echo "."
+echo "."
+echo "."
+echo "."
+echo "."
+echo "."
+lolcat "Jangan Lupa Subscribe Karena Saya Capek😢"
+echo "."
+echo "."
+echo "."
+echo "."
+echo "."
+echo "."
+echo "."
+echo "."
+echo "."
+echo "."
+pkg update && pkg upgrade
+pkg install m4
+pkg install macchina
+pkg install man
+pkg install mangal
+pkg install manpages
+pkg install mapserver
+pkg install marisa
+pkg install markdown-flashcards
+pkg install matplotlib
+pkg install matterbridge
+pkg install matterircd
+pkg install mautrix-whatsapp
+pkg install maven
+pkg install maxcso
+pkg install maxima
+pkg install mazter
+pkg install mc
+pkg install mcfly
+pkg install mdbtools
+pkg install mdns-scan
+pkg install mdp
+pkg install media-type
+pkg install mediainfo
+pkg install megacmd
+pkg install megatools
+pkg install meli
+pkg install mimetic
+pkg install minesweeper
+pkg install minicom
+pkg install minio
+pkg install minisign
+pkg install mlir
+pkg install mlocate
+pkg install moarvm
+pkg install mold
+pkg install monero
+pkg install monetdb
+pkg install mongodb
+pkg install mono
+pkg install monolith
+pkg install moon-buggy
+pkg install moreutils
+pkg install moria
+pkg install morse2ascii
+pkg install mosh-perl
+pkg install mosh
+pkg install mosquitto
+pkg install most
+pkg install mp3cat-go
+pkg install mp3cat
+pkg install mp3gain
+pkg install mp3splt
+pkg install mp3wrap
+pkg install mpc
+pkg install mpd
+pkg install mplayer
+pkg install mpv
+pkg install mtools
+pkg install mu
+pkg install music-file-organizer
+pkg install mutt
+pkg install myman
+pkg install mympd
+pkg install mypaint-brushes
+pkg install n2n
+pkg install nala
+pkg install nali
+pkg install nano
+pkg install navi
+pkg install navidrome
+pkg install ncdc
+pkg install ncdu2
+pkg install ncdu
+pkg install ncmpcpp
+pkg install ncompress
+pkg install ncpamixer
+pkg install ne
+pkg install nelua
+pkg install neofetch
+pkg install net-tools
+pkg install newsboat
+pkg install nginx
+pkg install ngircd
+pkg install ngspice
+pkg install nim
+pkg install ninja
+pkg install nmap
+pkg install nodejs
+pkg install nudoku
+pkg install nushell
+pkg install nwipe
+pkg install nyancat
+pkg install o-editor
+pkg install octomap
+pkg install opencolorio
+pkg install openfoam
+pkg install opengl
+pkg install openimageio
+pkg install openjpeg
+pkg install openldap
+pkg install openldap
+pkg install openmpi
+pkg install openscad
+pkg install openspecfun
+pkg install openssh
+pkg install openssl1.1
+pkg install openssl
+pkg install openvdb
+pkg install optipng
+pkg install opus-tools
+pkg install opusfile
+pkg install opusfile
+pkg install osm2pgsql
+pkg install osmctools
+pkg install osmesa
+pkg install osmium-tool
+pkg install osslsigncode
+pkg install ossp-uuid
+pkg install p7zip
+pkg install pacman-contrib
+pkg install pacman
+pkg install pacutils
+pkg install panda3d-samples
+pkg install panda3d
+pkg install pandoc
+pkg install pangopaperkey
+pkg install par2pari
+pkg install paruz
+pkg install pass-otp
+pkg install pastebinit
+pkg install pastel
+pkg install pathpicker
+pkg install pb
+pkg install pcal
+pkg install pcaudiolib
+pkg install pdf2svg
+pkg install pdfcpu
+pkg install pdftk
+pkg install peaclock
+pkg install peco
+pkg install perl-rename
+pkg install petpforth
+pkg install photon-rssphp7.2
+pkg install php7php
+pkg install phpmyadmin
+pkg install pianobar
+pkg install pick
+pkg install plantuml
+pkg install play-audio
+pkg install playit
+pkg install plutolang
+pkg install plzip
+pkg install pngcrush
+pkg install poke
+pkg install polipo
+pkg install polyml
+pkg install poppler
+pkg install portaudio
+pkg install portmidi
+pkg install potrace-sta
+pkg install povray
+pkg install procps
+pkg install procs
+pkg install procyon-decompiler
+pkg install profanity
+pkg install progress
+pkg install proj
+pkg install proot
+pkg install psmisc
+pkg install ptunnel
+pkg install pulseaudio
+puppygobject
+pkg install pypy3
+pkg install pypy
+pkg install pystring
+pkg install python2
+pkg install python3.10
+pkg install python3
+pkg install python3.9
+pkg install python
+pkg install pyunbound
+pkg install q-dns-client
+pkg install qrsspig
+pkg install quickjs
+pkg install rakudo
+pkg install rapidjson
+pkg install rclone
+pkg install rcshell
+pkg install rdrview
+pkg install re2c
+pkg install readline
+pkg install recode
+pkg install ripsecrets
+pkg install robin-map
+pkg install rpm
+pkg install rq
+pkg install rsgain
+pkg install rsnapshot
+pkg install rubiks-cube
+pkg install ruby-2
+pkg install ruby-is-ruby2
+pkg install ruby
+pkg install ruff
+pkg install runit
+pkg install rush
+pkg install rxfetch
+pkg install s-nail
+pkg install salty-chat
+pkg install samba
+pkg install samefile
+pkg install samurai
+pkg install sassc
+pkg install sc-im
+pkg install scala
+pkg install scc
+pkg install sccache
+pkg install scdaemon
+pkg install scdoc
+pkg install screen
+pkg install screenfetch
+pkg install sed
+pkg install seledroid-app
+pkg install senpai
+pkg install sfeed
+pkg install shark
+pkg install sslscan
+pkg install swaks
+pkg install swi-prolog
+pkg install swift-runtime-arm
+pkg install tar
+pkg install telegram-bot-api
+pkg install teleport-tsh
+pkg install termbot
+pkg install termimage
+pkg install termplay
+pkg install termux-api
+pkg install termux-apt-repo
+pkg install termux-auth
+pkg install termux-gui-bash
+pkg install termux-gui-c
+pkg install termux-gui-package
+pkg install termux-gui-pm
+pkg install termux-keyring
+pkg install termux-licenses
+pkg install termux-services
+pkg install termux-tools
+pkg install time
+pkg install topgrade
+pkg install tor
+pkg install torsocks
+pkg install translate-shell
+pkg install ttyper
+pkg install ttyplot
+pkg install ttyrec
+pkg install tur-repo
+pkg install turbo-text-editor
+pkg install unpaper
+pkg install unrar
+pkg install unshield
+pkg install unzip
+pkg install update-info-dir
+pkg install w3m-img
+pkg install weechat-perl-plugin
+pkg install weechat-python-plugin
+pkg install weechat-ruby-plugin
+pkg install weechat
+pkg install wego
+pkg install wget2
+pkg install wget
+pkg install wgetpaste
+pkg install wol
+pkg install wordgrinder
+pkg install wren
+pkg intsall wrk
+pkg install wtfutil
+pkg install wuzz
+pkg install wv-tools
+pkg install yarn
+pkg install yosys
+pkg install youtubedr
+pkg install ytfzf
+pkg install ytui-music
+pkg install yuma123
+pkg install zip
+pkg install zpaq
+pkg install zphisher
+lolcat "100+ berhasil di install"
---
+++ src/kecil.sh
@@ -0,0 +1,9 @@
+echo "Kecil ini"
+pkg update && pkg upgrade
+pkg install ruby
+pkg install git
+pkg install python
+pkg install python3
+pkg install python2
+pkg install nano
+pkg install neofetch
