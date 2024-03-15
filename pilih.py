6d8fb57dc1a5ff3fd2628d92672256f25d2d1c90
Kosong <160365589+OT4KK0SON9@users.noreply.github.com>
2024-03-15 17:06:20 +0700

    Add files via upload

---
+++ pilih.py
@@ -0,0 +1,44 @@
+import os,time,sys,shutil
+
+class Main:
+
+	def __init__(self):
+		self.detekos()
+
+	def menu(self):
+		print("""_____
+
+1.kecil
+2. sedang
+3. full
+4.backup
+- selamat puasa -
+____""")
+		pilih=int(input('apa/> '))
+		if pilih == 1:
+			import src.kecil
+		elif pilih == 2:
+			import src.sedang
+		elif pilih == 3:
+			import src.full
+			elif pilih == 4:
+			import src.backup
+		else: print("[!] Ngasal Jir(o)");self.menu()
+
+	def detekos(self):
+		#remove cache
+		try:
+			shutil.rmtree("src/__pycache__")
+		except: pass
+
+		if os.name in ['nt','win32']:
+			os.system('cls')
+		else: os.system('clear')
+		self.menu()
+
+try:
+	Main()
+except KeyboardInterrupt:
+	exit('[Exit] -->')
+except Exception as F:
+	print('Err: %s'%(F))
